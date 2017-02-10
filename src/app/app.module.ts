a748ec832776db83d6ac9073542192fc244e3bd5
The Buildbot <buildbot@angular2buch.de>
2017-02-10 11:31:03 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/3d34a89aef300d3d6d6f42e66e22d1d146879d62
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/200308405

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { BookStoreService } from './shared/book-store.service';
+import { AppRoutingModule } from './app-routing.module';
+import { BookResolver } from './shared/book-resolver.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    BookStoreService,
+    BookResolver,
+    { provide: LOCALE_ID, useValue: 'de' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
